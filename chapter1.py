952c8bcf86b0c73ab4898c58bc525a245baca46f
prince <princepriyadarshi2017@gmail.com>
2022-11-27 22:54:26 +0530

    in

--- chapter1.py
+++ chapter1.py
@@ -5,3 +5,25 @@ img =cv2.imread("resource/duragji.jpg")
 
 cv2.imshow("Output",img)
 cv2.waitKey(0)
+
+
+### how to read video
+cap=cv2.VideoCapture("resource/1.mp4")
+
+while True:
+    success, img=cap.read()
+    cv2.imshow("Video",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+### how to read webcam
+cap=cv2.VideoCapture(0)
+cap.set(3,640) #set is setting
+cap.set(4,480)
+cap.set(10,100)#changing brightness
+
+while True:
+    success, img=cap.read()
+    cv2.imshow("Video",img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
