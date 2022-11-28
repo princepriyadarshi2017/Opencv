f53e40f21c8b1249a58abd2ed98b2ff2c6123d11
prince <princepriyadarshi2017@gmail.com>
2022-11-28 11:29:50 +0530

    in

---
+++ chapter2.py
@@ -0,0 +1,11 @@
+import cv2
+ 
+img = cv2.imread("resource/duragji.jpg")
+
+imgGry=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gry Image",imgGry)
+cv2.waitKey(0)
+
+imgBlur=cv2.GaussianBlur(imgGry,(7,7),0)
+cv2.imshow("Blur Image",imgBlur)
+cv2.waitKey(0)
